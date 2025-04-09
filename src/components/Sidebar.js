671d7026293562493fc163d73ae75e3030be20ed
nexovanta <nexovantakelvin@gmail.com>
2025-04-09 01:14:00 +0000

    Update and rename Java Script to src/components/Sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Profile</li>
+        <li>Messages</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
