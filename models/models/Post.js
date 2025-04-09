14663bebd5270745481f7d741a00320811effcb6
nexovanta <nexovantakelvin@gmail.com>
2025-04-09 01:22:44 +0000

    Update and rename Script to models/models/Post.js

---
+++ models/models/Post.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String, required: false },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
